65aad4f64a748527744c6a74abb0a3722dcbdbac
simran <sk8873471@gmail.com>
2026-02-04 12:40:18 +0530

    add module 4 .py

--- PyCharmMiscProject/module 4.py
+++ PyCharmMiscProject/module 4.py
@@ -1,66 +1,70 @@
-# numbers divisible by three in the range 1 - 1000
+#while loop to print out all the numbers which are divisible by three in the range of 1-1000
 
 num = 1
 
 while num <= 1000:
     if num % 3 == 0:
         print(num)
-    num += 1
+    num = num + 1
 
 
 # convert inches to centimeters
 
-inches = float(input("Enter inches (negative to stop): "))
+inch = float(input("Enter inches (negative number to stop): "))
 
-while inches >= 0:
-    cm = inches * 2.54
+while inch >= 0:
+    cm = inch * 2.54
     print("Centimeters:", cm)
+    inch = float(input("Enter inches (negative number to stop): "))
 
-    inches = float(input("Enter inches (negative to stop): "))
+print("Program ended.")
 
 
-# smallest and largest numbers
+
+#smallest and largest number
 
 smallest = None
 largest = None
-
-num = input("Enter a number (press Enter to quit): ")
-
-while num != "":
-    number = float(num)
-
-    if smallest is None or number < smallest:
-        smallest = number
-
-    if largest is None or number > largest:
-        largest = number
-
+while True:
     num = input("Enter a number (press Enter to quit): ")
+
+    if num == "":
+        break
+
+    num = float(num)
+
+    if smallest is None or num < smallest:
+        smallest = num
+
+    if largest is None or num > largest:
+        largest = num
 
 print("Smallest number:", smallest)
 print("Largest number:", largest)
 
 
 
-# random integer between 1 and 10
+#guess the number
 
 import random
 
 number = random.randint(1, 10)
 
-guess = int(input("pick a number (1–10): "))
+guess = int(input("pick a number between 1 and 10: "))
 
 while guess != number:
     if guess > number:
         print("Too high")
     else:
         print("Too low")
-    guess = int(input("Guess again: "))
 
-print("Correct!")
+    guess = int(input("guess again: "))
+
+print("Correct! You guessed the number.")
 
 
-# username and password
+
+#username and password
 
 correct_username = "python"
 correct_password = "rules"
@@ -76,28 +80,31 @@ while attempts < 5:
         break
     else:
         print("Wrong username or password")
-        attempts += 1
+        attempts = attempts + 1
 
 if attempts == 5:
     print("Access denied")
 
 
 
-#random points to generate, and calculates the value of pi
+#approximate the value of pi using random points
 
 import random
 
-n = int(input("How many random points to generate? "))
+points = int(input("Enter number of points: "))
 
-inside_circle = 0
+inside = 0
+i = 0
 
-for i in range(n):
+while i < points:
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
 
     if x*x + y*y < 1:
-        inside_circle += 1
+        inside = inside + 1
 
-pi = 4 * inside_circle / n
+    i = i + 1
 
+pi = 4 * inside / points
 print("Approximate value of pi:", pi)
+
